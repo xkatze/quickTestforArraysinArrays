6535dd01616c3f8ebb1322db30cae16d849598ac
xkatze <hejhjepower@gmail.com>
2021-02-11 14:06:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-6vs4cw-zmusab
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-6vs4cw-zmusab)
---
+++ app/table-sorting-example.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, MatTableDataSource } from '@angular/material';
+
+
+@Component({
+  selector: 'table-sorting-example',
+  styleUrls: ['table-sorting-example.css'],
+  templateUrl: 'table-sorting-example.html',
+})
+export class TableSortingExample implements OnInit {
+  displayedColumns: string[] = ['_id', 'name'];
+  displayedColumns2: string[] = ['zonename', 'departmentname','wardsname','ipads'];
+  dataSource = new MatTableDataSource([]);
+  dataSource2 = new MatTableDataSource([]);
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngOnInit() {
+    var elements = [{ _id: 1, name: 'Paul Walker' },
+    { _id: 2, name: 'Lisa' }];
+
+    var elements2 = [
+      {zonename: "hello",
+       department: [{
+        departmentname: 1, 
+        wards: [
+          {wardsname: 3,
+            ipads: [
+              {nr: 1},
+              {nr: 12},
+              {nr: 4},
+              {nr: 5}
+            ]}
+        ]}
+      ]}]
+
+    var elements3 = [
+      {zonename: "hello",
+       department: [{
+        departmentname: 1, 
+        wards: [
+          {wardsname: 3,
+            ipads: [
+              {nr: 1}
+            ]}
+        ]}
+      ]}]
+
+    this.dataSource = new MatTableDataSource(elements);
+    this.dataSource2 = new MatTableDataSource(elements3);
+    this.dataSource.sort = this.sort;
+  }
+}
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
